5db78075f36ec147f31b51f75927de65088e7b3f
romya <royako@ttu.ee>
2023-03-08 20:49:47 +0200

    Trying to add my own components (failure)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+//import { createApp } from 'vue'
+//import App from './App.vue'
+import * as VueRouter from 'vue-router';
+import * as Vue from 'vue';
+import HelloSimple from './components/HelloSimple.vue'
 
-createApp(App).mount('#app')
+
+const router = VueRouter.createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: VueRouter.createWebHashHistory(),
+    routes: [
+        { path: '/', component: HelloSimple },
+        ] // short for `routes: routes`
+})
+const app = Vue.createApp({})
+// Make sure to _use_ the router instance to make the
+// whole app router-aware.
+app.use(router)
+
+app.mount('#app')
+
