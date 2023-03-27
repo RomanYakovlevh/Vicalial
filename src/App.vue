b7d7af0a3134bf17d6e38f3672d89e163361ff56
romanyakovlev <ryakovlev@evolution.com>
2023-03-27 16:07:28 +0300

    Its aliiiiveeeee!!

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/calculator">CalculatorView</router-link> |
+    <router-link to="/statement">Statement</router-link>
   </nav>
   <router-view/>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,26 +1,10 @@
-//import { createApp } from 'vue'
-//import App from './App.vue'
-import * as VueRouter from 'vue-router';
 import * as Vue from 'vue';
-//import HelloSimple from './components/HelloSimple.vue'
 import App from "@/App.vue";
-import StatementComponent from "@/components/StatementComponent.vue";
 import router from './router'
 import store from './store'
 
 
-const routes = VueRouter.createRouter({
-    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-    history: VueRouter.createWebHashHistory(),
-    routes: [
-        { path: '/', component: App },
-        { path: '/cv', component: StatementComponent}
-        ] // short for `routes: routes`
-})
-const app = Vue.createApp(App).use(store).use(router)
-// Make sure to _use_ the router instance to make the
-// whole app router-aware.
-app.use(routes)
+const app = Vue.createApp(App).use(router).use(store)
 
 app.mount('#app')
 
