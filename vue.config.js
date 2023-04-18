26f3749c8aced59dbb2334fff37b10b8821faab6
romanyakovlev <ryakovlev@evolution.com>
2023-04-18 15:28:46 +0300

    publicPath in vue.config.js change

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     headers: { 'Access-Control-Allow-Origin': '*' }
-  }
+  },
+  publicPath: '/~royako/iti0209/vicalial-vue/dist/'
 })
