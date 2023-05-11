a7106ae7faa02b49b4dadcc083969f0feaa3d0eb
romya <royako@ttu.ee>
2023-05-11 16:25:46 +0300

    generators

--- src/components/AddDataWindow.vue
+++ src/components/AddDataWindow.vue
@@ -9,7 +9,7 @@
         <v-card-text>
             <v-window v-model="tab">
                 <v-window-item value="Any expression">
-                    <AddAnyExpression @close-dialog="closeDialog" @new-statement-added="newStatementAdded"/>
+                    <AddAnyExpression @close-dialog="closeDialog" @new-statement-added="newStatementAdded" />
                 </v-window-item>
 
                 <v-window-item value="Manual input">
@@ -17,11 +17,47 @@
                 </v-window-item>
 
                 <v-window-item value="From file">
-                    <AddFromFile @close-dialog="closeDialog" @new-statement-added="newStatementAdded"/>
+                    <AddFromFile @close-dialog="closeDialog" @new-statement-added="newStatementAdded" />
                 </v-window-item>
 
                 <v-window-item value="Generator">
-                    Generator
+                    <v-card>
+                        <v-card-subtitle>Choose one of the generators, and sepcify dimensions of the
+                            result.</v-card-subtitle>
+                        <div class="d-flex flex-row">
+                            <v-text-field v-model="dim1" class="ma-2" label="Height"></v-text-field><v-text-field
+                                v-model="dim2" class="ma-2" label="Width"></v-text-field>
+                        </div>
+
+                        <v-tabs v-model="text" bg-color="primary">
+                            <v-tab value="Ones">Ones</v-tab>
+                            <v-tab value="Zeros">Zeros</v-tab>
+                            <v-tab value="Eye">Eye</v-tab>
+                            <v-tab value="Range">Range</v-tab>
+                        </v-tabs>
+                        <v-card-text>
+                            <v-window v-model="text">
+                                <v-window-item value="Ones">
+                                    <MatrixPreview :matrix="ones(dim1, dim2)"></MatrixPreview>
+                                </v-window-item>
+                                <v-window-item value="Zeros">
+                                    <MatrixPreview :matrix="zeros(dim1, dim2)"></MatrixPreview>
+                                </v-window-item>
+                                <v-window-item value="Eye">
+                                    <MatrixPreview :matrix="eye(dim1, dim2)"></MatrixPreview>
+                                </v-window-item>
+                                <v-window-item value="Range">
+                                    <MatrixPreview :matrix="range(dim1, dim2)"></MatrixPreview>
+                                </v-window-item>
+                            </v-window>
+                        </v-card-text>
+
+                        <v-card-actions class="buttons">
+                            <v-btn class="btn confirm" @click="addNewMatrixAndCloserWindow">Confirm</v-btn>
+                            <v-btn class="btn cancel" @click="closeDialog">Cancel</v-btn>
+                        </v-card-actions>
+
+                    </v-card>
                 </v-window-item>
             </v-window>
         </v-card-text>
@@ -31,15 +67,20 @@
 <script>
 import AddAnyExpression from './AddAnyExpression.vue'
 import AddFromFile from './AddFromFile.vue';
+import MatrixPreview from './MatrixPreview.vue';
 
 export default {
     name: "AddDataWindow",
     components: {
         AddAnyExpression,
-        AddFromFile
+        AddFromFile,
+        MatrixPreview
     },
     data: () => ({
         tab: null,
+        dim1: "3",
+        dim2: "3",
+        text: null
     }),
     methods: {
         closeDialog() {
@@ -47,6 +88,34 @@ export default {
         },
         newStatementAdded(statement) {
             this.$emit('newStatementAdded', statement)
+        },
+        ones(h, w) {
+            return Array(h).map(() => {
+                return Array(w).fill(1)
+            })
+        },
+        zeros(h, w) {
+            return Array(h).map(() => {
+                return Array(w).fill(0)
+            })
+        },
+        eye(h, w) {
+            return Array(h).map((x, i) => {
+                x;
+                return Array(w).map((y, j) => {
+                    y;
+                    if (i === j) {
+                        return 1
+                    } else {
+                        return 0
+                    }
+                })
+            })
+        },
+        range(h, w) {
+            return Array(h).map(() => {
+                return Range(w)
+            })
         }
     },
     emits: ['closeDialog', 'newStatementAdded']
---
+++ src/components/MatrixPreview.vue
@@ -0,0 +1,45 @@
+<template>
+    <table ref="matrix" class="matrix">
+        <tr v-for="(row, rowIndex) in matrix" :key="rowIndex">
+            <td v-for="(item, colIndex) in row"
+                :key="rowIndex * matrix.columnsAmount + colIndex">
+                <div class="hld">{{ item }}</div>
+            </td>
+        </tr>
+    </table>
+</template>
+
+<script>
+
+export default {
+    name: 'MatrixPreview',
+    props: {
+        matrix: { required: true }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.matrix {
+    margin: 0% 1% 1% 1%;
+    border: 0px solid #999999;
+    font-family: 'Roboto Mono', monospace;
+    height: fit-content;
+    align-self: center;
+}
+
+.row-col-selector-btn {
+    border: none;
+    border-radius: 5px;
+    width: 2em;
+    height: 2em;
+}
+
+.row-col-selector-btn:hover {
+    background-color: #d6d5d2;
+}
+
+.hld {
+    margin: 3px 8px 3px 8px;
+}
+</style>
