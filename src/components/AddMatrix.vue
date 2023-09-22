bf8236230aa20414967a76681a3bf895d1c8daa8
romya <rom.yakovlev01@gmail.com>
2023-09-23 01:47:54 +0300

    addmatrix component changes

--- src/components/AddMatrix.vue
+++ src/components/AddMatrix.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="d-flex flex-row">
     <v-text-field
+      :rules="[rules.isNumber]"
       v-model="dim1"
       class="ma-2"
       label="Height"
-      :rules="[isNumber]"
     ></v-text-field>
     <v-text-field
+      :rules="[rules.isNumber]"
       v-model="dim2"
       class="ma-2"
       label="Width"
-      :rules="[isNumber]"
     ></v-text-field>
   </div>
-  <div ></div>
+  <div></div>
   <table ref="matrix" class="matrix" style="border: 1px solid black">
     <tr v-for="i in Number(dim1)" :key="i">
       <td v-for="j in Number(dim2)" :key="j">
-        <div class="hld">|{{ i }},{{ j }}|</div>
+        <div class="hld">
+          <v-text-field></v-text-field>
+        </div>
       </td>
     </tr>
   </table>
@@ -27,19 +29,22 @@
 export default {
   name: "AddMatrix",
   methods: {
-    isNumber(v) {
-      const t = !isNaN(Number(v));
-      if (t) {
-        return true;
-      } else {
-        return "Only numericals are allowed.";
-      }
-    },
   },
   data() {
     return {
       dim1: 3,
       dim2: 3,
+      rules: {
+        isNumber: v => {
+          const t = isNaN(Number(v))
+          console.log(t)
+          if (t) {
+            return 'Only numericals are allowed.'
+          } else {
+            return true
+          }
+        },
+      },
     };
   },
   emits: [],
